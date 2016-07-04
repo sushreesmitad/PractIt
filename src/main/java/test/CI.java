fe454c3ee9b47f8ad574ac7801c2fa87663b2a37
sushreesmitad <sushreesmita.dhal13@gmail.com>
2016-07-04 13:17:03 +0530

    ci test

---
+++ src/main/java/test/CI.java
@@ -0,0 +1,22 @@
+package test;
+import static java.lang.System.*;    
+public class CI {
+	
+	
+	public void method(String s) {
+
+		System.out.println("string is " + s);
+
+	}
+
+	public static void main(String... args) {
+
+		CI ci = new CI();
+
+		ci.method("10");
+		/* lineSeparator() */
+
+	}
+	}
+
+
---
+++ src/test/java/test/TestCI.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+public class TestCI {
+	
+	
+	
+	@Test
+	
+	public void testCI() {
+	}
+	}
+
+
